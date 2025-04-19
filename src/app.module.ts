1bd14db0b47cb9a8d2dc7b36cf35ff489cc0f4da
e.nagornyak <dolkiv@gmail.com>
2025-04-19 14:43:39 +0300

    feat: remove old code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { LogModule } from './features/logs/log.module'
-import { UsersModule } from './features/users/users.module'
 import { ThrottlerModule } from '@nestjs/throttler'
 import { AuthGuard } from './common/quards/auth-guard'
-import { SettingsModule } from './features/settings/settings.module';
 
 @Module({
   imports: [
@@ -17,9 +14,6 @@ import { SettingsModule } from './features/settings/settings.module';
       ],
     }),
     ConfigModule.forRoot({ isGlobal: true }),
-    LogModule,
-    UsersModule,
-    SettingsModule,
   ],
   providers: [AuthGuard],
   exports: [AuthGuard],
